f792f068cee70cf41182bc5253c64b72dfa2369d
HarsimranMann <82723261+HarsimranMann@users.noreply.github.com>
2021-06-18 20:28:37 -0400

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,10 @@
+class Box extends BaseClass {
+constructor(x,y,width,height){
+//to call constuctor of baseClass we use super
+super(x,y,width,height)
+
+this.image = loadImage("sprites/wood1.png")
+
+}
+
+}
